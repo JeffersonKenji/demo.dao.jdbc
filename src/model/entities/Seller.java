c9027a82c995bf30aad3329d64cbd8ae9eb7eb89
JeffersonKenji <jepesi77@gmail.com>
2019-12-02 22:09:46 -0300

    update implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -7,20 +7,15 @@ public class Seller implements Serializable {
 	
 
 	private static final long serialVersionUID = 1L;
-	private int id;
+	private Integer id;
 	private String name;
 	private String email;
 	private Date birthDate;
 	private Double baseSalary;
 	
 	private Department department;
-	
-	public Seller() {
-		
-	}
 
-	public Seller(int id, String name, String email, Date birthDate, Double baseSalary, Department department) {
-		
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
